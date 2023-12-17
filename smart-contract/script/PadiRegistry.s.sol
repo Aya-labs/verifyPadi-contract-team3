de8f4dcc776f795bea73942d5dfe9bc3467e3a3a
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-12-17 13:08:06 +0100

    deploy script added

---
+++ smart-contract/script/PadiRegistry.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {PadiRegistry} from "../src/PadiRegistry.sol";
+
+contract DeployPadiRegistry is Script {
+
+    function setUp() public {}
+
+    function run() public {
+        // Replace this with your desired Padi Address
+        // address padiAddress = 0x...;
+
+        // PadiRegistry registry = new PadiRegistry(padiAddress);
+        // console2.log("Deployed PadiRegistry to:", address(registry));
+
+        // Uncomment and modify these lines to perform post-deployment actions
+        // registry.createUser();
+        // registry.addVerifier(address(0x...));
+        // ...
+
+        uint privateKey = vm.envUint("PRIVATE_KEY");
+        address account = vm.addr(privateKey);
+
+        console2.log("Deployed PadiRegistry to: ", account);
+
+        vm.startBroadcast(privateKey);
+        new PadiRegistry(padiAddress);
+        vm.stopBroadcast();
+    }
+}
