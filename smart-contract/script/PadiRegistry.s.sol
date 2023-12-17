7b3111fa64873dbbfc3f4373b895dd4076731849
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-12-17 13:54:00 +0100

    deployed successfully

--- smart-contract/script/PadiRegistry.s.sol
+++ smart-contract/script/PadiRegistry.s.sol
@@ -9,24 +9,16 @@ contract DeployPadiRegistry is Script {
     function setUp() public {}
 
     function run() public {
-        // Replace this with your desired Padi Address
-        // address padiAddress = 0x...;
 
-        // PadiRegistry registry = new PadiRegistry(padiAddress);
-        // console2.log("Deployed PadiRegistry to:", address(registry));
-
-        // Uncomment and modify these lines to perform post-deployment actions
-        // registry.createUser();
-        // registry.addVerifier(address(0x...));
-        // ...
-
-        uint privateKey = vm.envUint("PRIVATE_KEY");
-        address padiAddress = vm.addr(privateKey);
+        bytes memory privateKeyBytes = bytes(vm.envString("PRIVATE_KEY"));
+        bytes32 privateKey = bytes32(uint256(keccak256(privateKeyBytes)));
+        address padiAddress = address(uint160(uint256(privateKey)));
 
         console2.log("Deployed PadiRegistry to: ", padiAddress);
-
-        vm.startBroadcast(privateKey);
+        
+        vm.startBroadcast();
         new PadiRegistry(padiAddress);
         vm.stopBroadcast();
+
     }
 }
